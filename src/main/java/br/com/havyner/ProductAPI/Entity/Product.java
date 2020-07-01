5e343c43449ce9648873f9814a8e517c72a7a98e
Havyner Caetano <hrcgeo@gmail.com>
2020-06-30 22:00:54 -0300

    Add Lombok library

--- src/main/java/br/com/havyner/ProductAPI/Entity/Product.java
+++ src/main/java/br/com/havyner/ProductAPI/Entity/Product.java
@@ -1,9 +1,13 @@
 package br.com.havyner.ProductAPI.Entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
 @Table(name = "Products")
+@NoArgsConstructor
+@Data
 public class Product {
 
     @Id
@@ -25,62 +29,4 @@ public class Product {
 
     @Column(name = "quantity")
     private Integer quantity;
-
-    public Product() {}
-
-    public Product(String name, String description, String manufacturer, Double price, Integer quantity) {
-        this.name = name;
-        this.description = description;
-        this.manufacturer = manufacturer;
-        this.price = price;
-        this.quantity = quantity;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getManufacturer() {
-        return manufacturer;
-    }
-
-    public void setManufacturer(String manufacturer) {
-        this.manufacturer = manufacturer;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
 }
