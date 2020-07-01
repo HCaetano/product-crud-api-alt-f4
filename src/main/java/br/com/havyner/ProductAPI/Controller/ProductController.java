92a272e9b0688f151cb95bd5ca18981343effd66
Havyner Caetano <hrcgeo@gmail.com>
2020-07-01 10:46:53 -0300

    Add Swagger annotations to ProductController

--- src/main/java/br/com/havyner/ProductAPI/Controller/ProductController.java
+++ src/main/java/br/com/havyner/ProductAPI/Controller/ProductController.java
@@ -2,43 +2,50 @@ package br.com.havyner.ProductAPI.Controller;
 
 import br.com.havyner.ProductAPI.Entity.Product;
 import br.com.havyner.ProductAPI.Service.ProductService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @Controller
 @RequestMapping("/api/products")
+@Api(value = "Product REST API")
 public class ProductController {
 
     @Autowired
     ProductService service;
 
+    @ApiOperation(value = "Adds a product to the database")
     @PostMapping
     @ResponseBody
     public Product save(@RequestBody Product product) {
         return service.save(product);
     }
 
+    @ApiOperation(value = "Updates info about a product")
     @PutMapping(value = "/{id}")
     @ResponseBody
     public Product update(@RequestBody Product product, @PathVariable Integer id) {
         return service.update(product, id);
     }
 
+    @ApiOperation(value = "Removes a product from the database")
     @DeleteMapping(value = "/{id}")
     @ResponseBody
     public void delete(@PathVariable Integer id) {
         service.delete(id);
     }
 
+    @ApiOperation(value = "Shows all products in the database")
     @GetMapping
     @ResponseBody
     public List<Product> findAll() {
         return service.findAll();
     }
 
+    @ApiOperation(value = "Shows a specific product, retrieved by its id")
     @GetMapping(value = "/{id}")
     @ResponseBody
     public Product findById(@PathVariable Integer id) {
